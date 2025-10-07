93c5318551a44b89ffd122d6b003ec063e2ec67d
Trinh Minh Tuan <trinhmihntuan312005@gmail.com>
2025-10-08 01:39:51 +0700

    first commit - socketchat

--- server.py
+++ server.py
@@ -12,9 +12,10 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f"📩 {msg}")
+    print(f"📩 Nhận tin nhắn: {msg}")
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    print(f"✅ Server đang chạy tại http://localhost:{port}")
+    socketio.run(app, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
